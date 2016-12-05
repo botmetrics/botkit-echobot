8da21379f949f41142b7cd70f4363dfee355e1b7
arunthampi <arun.thampi@gmail.com>
2016-12-05 09:19:59 -0800

    Setup with Foreman

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-if (!process.env.page_token) {
+if (!process.env.PAGE_ACCESS_TOKEN) {
     console.log('Error: Specify page_token in environment');
     process.exit(1);
 }
 
-if (!process.env.verify_token) {
+if (!process.env.VERIFY_TOKEN) {
     console.log('Error: Specify verify_token in environment');
     process.exit(1);
 }
@@ -11,14 +11,13 @@ if (!process.env.verify_token) {
 var Botkit = require('botkit');
 
 var controller = Botkit.facebookbot({
-    debug: true,
-    access_token: process.env.page_token,
-    verify_token: process.env.verify_token
+    access_token: process.env.PAGE_ACCESS_TOKEN,
+    verify_token: process.env.VERIFY_TOKEN
 });
 
 var bot = controller.spawn({});
 
-controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
+controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
     });
